411e0aa028da2a049f728ed01ee64ab28ba2cce3
Wachacha-jay <jameswachacha@gmail.com>
2023-04-11 16:12:38 +0000

    loops

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# 97 in char is a
+# 122 in char is z
+for i in range (97, 123):
+    print("{}" .format(chr(i)), end=" ")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+practice on if, else, for and while loops
