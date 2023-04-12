ba80470b6e9a37f7a40a3b6ee52d3e15d71fad77
Wachacha-jay <jameswachacha@gmail.com>
2023-04-12 21:02:05 +0000

    modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__name__":
+    import sys
+    result = 0
+    if (len(sys.argv) > 1):
+        for i in range (1, len(sys.argv)):
+            sum += (int(sys.argv[i]))
+    print("{}" .format(result))
