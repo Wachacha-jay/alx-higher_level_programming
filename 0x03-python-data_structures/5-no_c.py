76e7f95b03e92d8515155c3626f4c2ddb0ce419a
Wachacha-jay <jameswachacha@gmail.com>
2023-04-17 21:49:20 +0000

    data st ructures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    print(" " .join(c for c not in my_string if c not in "Cc"))
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            if i is not row[len(row) - 1]:
+                print("{}" .format(i), end=" ")
+            else:
+                print("{}" .format(i), end=" ")
+        print()
